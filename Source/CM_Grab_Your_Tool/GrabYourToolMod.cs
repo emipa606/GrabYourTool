8ce31daf60ac1585db876262645c5a42cce63bed
NathanHarbour <nathanharbour@gmail.com>
2021-01-20 21:42:05 -0600

    Refactored tool switching to allow for auto-un-switching

--- Source/CM_Grab_Your_Tool/GrabYourToolMod.cs
+++ Source/CM_Grab_Your_Tool/GrabYourToolMod.cs
@@ -18,7 +18,7 @@ namespace CM_Grab_Your_Tool
 
         public static bool UsingCombatExtended => ModsConfig.ActiveModsInLoadOrder.Any((ModMetaData m) => m.Name == "Combat Extended");
 
-        public Dictionary<Pawn, GrabYourToolMemory> pawnMemory = new Dictionary<Pawn, GrabYourToolMemory>();
+        public ToolMemoryTracker ToolMemories => Current.Game.World.GetComponent<ToolMemoryTracker>();
 
         public GrabYourToolMod(ModContentPack content) : base(content)
         {
@@ -30,45 +30,17 @@ namespace CM_Grab_Your_Tool
 
         public bool IsPawnUsingTool(Pawn pawn)
         {
-            return (pawnMemory.ContainsKey(pawn) && pawnMemory[pawn].IsUsingTool);
+            return ToolMemories?.GetMemory(pawn)?.IsUsingTool ?? false;
         }
 
-        public GrabYourToolMemory GetMemory(Pawn pawn)
+        public ToolMemory GetMemory(Pawn pawn)
         {
-            if (!pawnMemory.ContainsKey(pawn))
-                pawnMemory.Add(pawn, new GrabYourToolMemory());
-
-            return pawnMemory[pawn];
+            return ToolMemories?.GetMemory(pawn);
         }
 
         public void ClearMemory(Pawn pawn)
         {
-            if (pawnMemory.ContainsKey(pawn))
-                pawnMemory.Remove(pawn);
-        }
-    }
-
-    public class GrabYourToolMemory
-    {
-        private SkillDef lastCheckedSkill = null;
-        private bool? usingTool = false;
-
-        public bool IsUsingTool => (usingTool.HasValue ? usingTool.Value : false);
-
-        public bool UpdateSkill(SkillDef skill)
-        {
-            if (lastCheckedSkill != skill)
-            {
-                lastCheckedSkill = skill;
-                return true;
-            }
-
-            return false;
-        }
-
-        public void UpdateUsingTool(bool isUsingTool)
-        {
-            usingTool = isUsingTool;
+            ToolMemories?.ClearMemory(pawn);
         }
     }
 
@@ -104,118 +76,32 @@ namespace CM_Grab_Your_Tool
 
                     if (activeSkill != null)
                     {
-                        GrabYourToolMemory memory = GrabYourToolMod.Instance.GetMemory(pawn);
+                        ToolMemory memory = GrabYourToolMod.Instance.GetMemory(pawn);
 
                         if (!memory.UpdateSkill(activeSkill))
                             return;
 
                         // Don't do it if this job uses weapons (i.e. hunting)
                         if (activeSkill == SkillDefOf.Shooting || activeSkill == SkillDefOf.Melee)
-                            memory.UpdateUsingTool(false);
+                        {
+                            memory.UpdateUsingTool(null, false);
+                        }
                         // Check currently equipped item
-                        else if (pawn.equipment.Primary != null && HasReleventStatModifiers(pawn.equipment.Primary, activeSkill))
-                            memory.UpdateUsingTool(true);
+                        else if (pawn.equipment.Primary != null && ToolMemoryTracker.HasReleventStatModifiers(pawn.equipment.Primary, activeSkill))
+                        {
+                            memory.UpdateUsingTool(null, true);
+                        }
                         // Try and find something else in inventory
                         else
-                            memory.UpdateUsingTool(EquipAppropriateWeapon(pawn, activeSkill));
+                        {
+                            memory.UpdateUsingTool(pawn.equipment.Primary, ToolMemoryTracker.EquipAppropriateWeapon(pawn, activeSkill));
+                        }
                     }
                     else
                     {
                         GrabYourToolMod.Instance.ClearMemory(pawn);
                     }
                 });
-            }
-
-            private static bool EquipAppropriateWeapon(Pawn pawn, SkillDef skill)
-            {
-                //Log.Message("TryActuallyStartNextToil - EquipAppropriateWeapon");
-                ThingOwner heldThingsOwner = pawn.inventory.GetDirectlyHeldThings();
-                List<Thing> weaponsHeld = heldThingsOwner.Where(thing => thing.def.IsWeapon).ToList();
-
-                foreach (Thing weapon in weaponsHeld)
-                {
-                    if (HasReleventStatModifiers(weapon, skill))
-                    {
-                        return TryEquipWeapon(pawn, weapon as ThingWithComps);
-                    }
-                }
-
-                return false;
-            }
-
-            private static bool HasReleventStatModifiers(Thing weapon, SkillDef skill)
-            {
-                List<StatModifier> statModifiers = weapon.def.equippedStatOffsets;
-                if (skill != null && statModifiers != null)
-                {
-                    //Logger.MessageFormat(this, "Found relevantSkills...");
-                    foreach (StatModifier statModifier in statModifiers)
-                    {
-                        List<SkillNeed> skillNeedOffsets = statModifier.stat.skillNeedOffsets;
-                        List<SkillNeed> skillNeedFactors = statModifier.stat.skillNeedFactors;
-
-                        if (skillNeedOffsets != null)
-                        {
-                            //Logger.MessageFormat(this, "Found skillNeedOffsets...");
-                            foreach (SkillNeed skillNeed in skillNeedOffsets)
-                            {
-                                if (skill == skillNeed.skill)
-                                {
-                                    //Logger.MessageFormat(this, "{0} has {1}, relevant to {2}", weapon.Label, statModifier.stat.label, skillNeed.skill);
-                                    return true;
-                                }
-                            }
-                        }
-
-                        if (skillNeedFactors != null)
-                        {
-                            foreach (SkillNeed skillNeed in skillNeedFactors)
-                            {
-                                if (skill == skillNeed.skill)
-                                {
-                                    //Logger.MessageFormat(this, "{0} has {1}, relevant to {2}", weapon.Label, statModifier.stat.label, skillNeed.skill);
-                                    return true;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                return false;
-            }
-
-            private static bool TryEquipWeapon(Pawn pawn, ThingWithComps weapon)
-            {
-                if (weapon == null)
-                    return false;
-
-                ThingWithComps currentWeapon = pawn.equipment.Primary;
-
-                bool transferSuccess = true;
-
-                if (currentWeapon != null)
-                    transferSuccess = pawn.inventory.innerContainer.TryAddOrTransfer(currentWeapon);
-
-                if (transferSuccess)
-                {
-                    if (weapon.stackCount > 1)
-                    {
-                        weapon = (ThingWithComps)weapon.SplitOff(1);
-                    }
-                    if (weapon.holdingOwner != null)
-                    {
-                        weapon.holdingOwner.Remove(weapon);
-                    }
-                    pawn.equipment.AddEquipment(weapon);
-                    weapon.def.soundInteract?.PlayOneShot(new TargetInfo(pawn.Position, pawn.Map));
-                    return true;
-                }
-                else
-                {
-                    Log.Warning("CM_Grab_Your_Tool: Unable to transfer equipped weapon to inventory");
-                }
-
-                return false;
             }
         }
     }
